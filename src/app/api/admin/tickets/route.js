faf444642814efe6baff5c597fcc88366777020a
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2025-06-16 10:48:21 +0600

    added the tickets functionality

--- src/app/api/admin/tickets/route.js
+++ src/app/api/admin/tickets/route.js
@@ -28,7 +28,32 @@ export const GET = async () => {
     await query(createTableQuery);
   }
   const result = await query(
-    "SELECT * FROM tickets where status = 'Pending' ORDER BY createdAt DESC"
+    "SELECT * FROM tickets where status = 'Pending' or status = 'In Progress' or status = 'Resolved' ORDER BY createdAt DESC"
   );
   return NextResponse.json(result);
+};
+
+export const PUT = async (req) => {
+  const { id } = await req.json();
+  // First check ticket status is not resolved
+  const checkStatusQuery = `SELECT status FROM tickets WHERE id = ${id}`;
+  const status = await query(checkStatusQuery);
+  if (status.rows[0].status === "Resolved") {
+    return NextResponse.json({ message: "Ticket is already resolved" });
+  } else if (status.rows[0].status === "In Progress") {
+    return NextResponse.json({ message: "Ticket is already in progress" });
+  } else if (status.rows[0].status === "Pending") {
+    const updateQuery = `UPDATE tickets SET status = 'In Progress' WHERE id = ${id}`;
+    await query(updateQuery);
+    return NextResponse.json({
+      message: "Ticket status updated to in progress",
+    });
+  }
+};
+
+export const DELETE = async (req) => {
+  const { id } = await req.json();
+  const deleteQuery = `DELETE FROM tickets WHERE id = ${id}`;
+  await query(deleteQuery);
+  return NextResponse.json({ message: "Ticket deleted successfully" });
 };
